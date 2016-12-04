840dc325f4d6e8a6dd63b44cd870b95dc84a3511
Alex Bondrea <sabondrea1@sheffield.ac.uk>
2016-12-04 22:59:21 +0000

    Added start_game method in pseudocode and is_a_color? method

--- floodit.rb
+++ floodit.rb
@@ -1,4 +1,5 @@
 require 'console_splash'
+require './launcher'
 
 # Public: Creates a randomly colored board for the game.
 #
@@ -7,7 +8,7 @@ require 'console_splash'
 #
 # Example:
 #
-#    get_board(2,2)  # => [[green,red],[cyan,blue]]
+# get_board(2,2)  # => [[green,red],[cyan,blue]]
 #
 # Returns a 2d array with random colour symbols
 def get_board(width, height)
@@ -21,5 +22,64 @@ def get_board(width, height)
       board[i][j] = colors.sample
     end
   end
+
   return board
 end
+
+# Public: Checks if the argument is a valid color
+#
+# input - symbol : a single letter
+#
+# Example:
+#
+# is_a_color?(:b) # => true
+#
+# Returns true if the color is valid and false otherwise
+def is_a_color?(input)
+  return  input == :r ||
+          input == :b ||
+          input == :g ||
+          input == :y ||
+          input == :c ||
+          input == :m
+end
+
+def update(board, player_color, input, current_completition)
+end
+
+#begin WORK IN PROGRESS
+def start_game(columns,rows,splash)
+  board = get_board(columns,rows)
+  turns = 0
+  current_completition = 0
+  player_color = board[0][0]
+  loop do
+    clear_game_screen()
+    display_board(board)
+    puts "Number of turns: #{turns}"
+    puts "Current completition: #{current_completition}%"
+    print "Choose a color: "
+    input = gets.chomp.to_sym
+    if is_a_color?(input)
+      board = update(board, player_color, input, current_completition)
+      display_board(splash,board)
+      turns += 1
+    elsif input == :q
+      break
+    else
+      print "Invalid input, please select a valid color"
+    end
+
+    break if is_over(board) == false
+  end
+end
+#end
+
+
+# 1.Create new board
+# 2.Display number of turns, % completed and ask for user input r,g,b,y,m,c
+# 3.Check if game_is_won
+# 3.1.If yes check if number of turns > high Score
+# 3.1.1.If yes update high Score
+# 3.2.If not check if user input is valid(color is in range and color exists)
+# 3.2.1. If yes,
